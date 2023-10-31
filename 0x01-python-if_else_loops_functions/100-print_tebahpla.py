6b0f9bf7f6da2169067c9b5943e44e476e5b57e3
Yara-ib <yr.ap.23@gmail.com>
2023-11-01 00:34:00 +0200

    islower

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# alphabet = "zYxWvUtSrQpOnMlKjIhGfEdCbA"
+for alphabet in range("zYxWvUtSrQpOnMlKjIhGfEdCbA"):
+    print(alphabet)
+
+
+# for alphabet in range(ord("z"),ord("a")):
+#     if alphabet % 2 == 0:
+#         print(alphabet.capitalize())
+#     print(alphabet)
+
+    # if alphabet in range((ord("z"), ord("a"))):
+    #     print(alphabet, end="")
+
+    # elif alphabet in range((ord("Z"), ord("A"))):
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str in range(ord("A"), ord("Z"))
+    for char in str:
+        print(str(char))
