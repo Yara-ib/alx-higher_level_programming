63a133d095d36e593985ca34ec54eecc33476f74
Yara-ib <yr.ap.23@gmail.com>
2023-10-31 18:14:23 +0200

    p_az

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alphabet in range(97,123):
-    print("{alphabet}".format(alphabet = chr(alphabet)), end="")
+for alphabet in range(97, 123):
+    print("{alphabet}".format(alphabet=chr(alphabet)), end="")
