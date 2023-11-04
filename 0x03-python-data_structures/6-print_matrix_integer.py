7696208dda4d410de133ee74e629c60cff5f9a98
Yara-ib <yr.ap.23@gmail.com>
2023-11-04 11:05:58 +0200

    Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for first in matrix:
         for second in first:
-            print("{:d}".format(second), end=" ")
+            if second is first[len(first) - 1]:
+                print("{:d}".format(second), end="")
+            else:
+                print("{:d}".format(second), end=" ")
         print()
