8a9c5448c1bb37edc59b75182e9402ceb39b2c54
Yara-ib <yr.ap.23@gmail.com>
2023-11-04 16:29:56 +0200

    Tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-    # print(help(tuple))
-    # tuple_a[i]
-    # if len(tuple_a) > 2:
-    #     tuple_a = tuple_a[:2]
-    if len(tuple_b) == 1:
-            tuple_b.extend(0)
+# def add_tuple(tuple_a=(), tuple_b=()):
+#     # print(help(tuple))
+#     # tuple_a[i]
+#     # if len(tuple_a) > 2:
+#     #     tuple_a = tuple_a[:2]
+#     if len(tuple_b) == 1:
+#             tuple_b.extend(0)
     # if len(tuple_a) < 2 and len(tuple_b) < 2:
     #     tuple_a.extend(0)
     #     tuple_b.extend(0)
 
     # elif len(tuple_a) < 2:
     #     tuple_b[1] = 0
-    x = int(tuple_a[0]) + int(tuple_b[0])
-    y = int(tuple_a[1]) + int(tuple_b[1])
-    tuple_a = (x, y)
-    return tuple_a
+#     x = int(tuple_a[0]) + int(tuple_b[0])
+#     y = int(tuple_a[1]) + int(tuple_b[1])
+#     tuple_a = (x, y)
+#     return tuple_a
 
 
 
@@ -25,3 +25,46 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # return res
     # if len(tuple_a) > 2 or len(tuple_b) > 2:
     #     tuple_a =
+
+
+# def add_tuple(tuple_a=(), tuple_b=()):
+
+#     if len(tuple_a) > 2:
+#         tuple_a = tuple_a[:2]
+#     if len(tuple_b) == 1:
+#             tuple_b.extend(0)
+#     if len(tuple_a) < 2 and len(tuple_b) < 2:
+#         tuple_a.extend(0)
+#         tuple_b.extend(0)
+
+#     elif len(tuple_a) < 2:
+#         tuple_b[1] = 0
+#     x = int(tuple_a[0]) + int(tuple_b[0])
+#     y = int(tuple_a[1]) + int(tuple_b[1])
+#     tuple_a = (x, y)
+#     return tuple_a
+# def add_tuple(tuple_a=(), tuple_b=()):
+#     if len(tuple_a) > 2:
+#         tuple_a = tuple_a[:2]
+#     if len(tuple_b) == 1:
+#         tuple_b = tuple_b + (0,)
+#     if len(tuple_a) < 2:
+#         tuple_a = tuple_a + (0,)
+#     if len(tuple_b) < 2:
+#         tuple_b = tuple_b + (0,)
+
+#     x = int(tuple_a[0]) + int(tuple_b[0])
+#     y = int(tuple_a[1]) + int(tuple_b[1])
+#     tuple_a = (x, y)
+#     return tuple_a
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
+
+    x = int(tuple_a[0]) + int(tuple_b[0])
+    y = int(tuple_a[1]) + int(tuple_b[1])
+    result_tuple = (x, y)
+    return result_tuple
