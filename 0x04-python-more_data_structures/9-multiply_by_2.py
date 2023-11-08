73c304e8fed4a1d92929bc40dac9f066ca4f7567
Yara-ib <yr.ap.23@gmail.com>
2023-11-09 01:58:55 +0200

    search

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def multiply_by_2(a_dictionary):
-# Write a function that returns a new dictionary with all values multiplied by 2
-    return map(lambda item: item * 2, a_dictionary.values())
