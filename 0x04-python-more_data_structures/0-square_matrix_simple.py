c856fc0c8f6c8216dedaf3d119c983ce9ae5d2c1
Yara-ib <yr.ap.23@gmail.com>
2023-11-08 23:01:35 +0200

    ls

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda crust: list(map(lambda core: core ** 2, crust)), matrix))
+    x = map(lambda crust: list(map(lambda core: core ** 2, crust)), matrix)
+    return list(x)
