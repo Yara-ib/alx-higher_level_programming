aa34ec448c53b642e206b969f47231602e2ccb36
Yara-ib <yr.ap.23@gmail.com>
2023-11-09 00:24:31 +0200

    Wtever will be will be

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
+
+
+# Write a function that deletes a key in a dictionary.
+# Prototype: def simple_delete(a_dictionary, key=""):
+# key argument will be always a string
+# If a key doesn’t exist, the dictionary won’t change
+# You are not allowed to import any module
