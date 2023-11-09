fb43d927e063ad09fd2a60e801b41ae43d6f12dc
Yara-ib <yr.ap.23@gmail.com>
2023-11-09 03:03:32 +0200

    pycodestyle *.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(a_dictionary)
+        x = sorted(a_dictionary.values())[-1]
+        return list(a_dictionary.keys())[list(a_dictionary.values()).index(x)]
