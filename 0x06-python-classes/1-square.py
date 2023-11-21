b8354b0eb7ef4552c5518164f2f478166f393084
Yara-ib <yr.ap.23@gmail.com>
2023-11-22 01:43:24 +0200

    size_sQuare!

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" An empty class defines a square. """
+
+
+class Square:
+    """ Return nothing .. """
+    pass
+
+    def __init__(self, __size):
+        """ Initializing the attributes. """
+        self._Square__size = __size
