26e7077e8bd292197af9a65e51164c2bfd775fc9
Yara-ib <yr.ap.23@gmail.com>
2023-11-22 22:58:09 +0200

    ByteCode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -16,7 +16,7 @@ class MagicClass:
             nothing.
         """
         self._MagicClass__radius = 0
-        if type(radius) is not int or type(radius) is not float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         else:
             self._MagicClass__radius = radius
