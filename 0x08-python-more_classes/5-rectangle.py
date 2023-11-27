1a8db0698100e0fc604f37259ff79474dd43abd2
Yara-ib <yr.ap.23@gmail.com>
2023-11-27 21:50:00 +0200

    Del

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -64,4 +64,5 @@ class Rectangle:
         return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
+        """ Delete instance of Rectangle and printing message during the process. """
         print("Bye rectangle...")
