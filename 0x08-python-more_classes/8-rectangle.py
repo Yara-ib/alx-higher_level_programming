fd587bd9f601123cf622b9b3b9bd963acb9085e6
Yara-ib <yr.ap.23@gmail.com>
2023-11-28 02:49:31 +0200

    Static

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -76,10 +76,9 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Method to return the biggest rectangle in area. """
-
-        if not eval(str(Rectangle.area(rect_1))):
+        if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not eval(str(Rectangle.area(rect_2))):
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
 
         if Rectangle.area(rect_1) >= Rectangle.area(rect_2):
