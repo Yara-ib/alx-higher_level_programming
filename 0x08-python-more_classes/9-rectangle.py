84ab05fd89749666a146284b0aef350137e47898
Yara-ib <yr.ap.23@gmail.com>
2023-11-28 03:17:44 +0200

    sQuare

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -7,6 +7,11 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
+    @classmethod
+    def square(cls, size=0):
+        """ Returns new instances to be squared shape. """
+        return cls(size, size)
+
     def __init__(self, width=0, height=0):
         """ Initializing parameters.
 
@@ -86,8 +91,3 @@ class Rectangle:
             return rect_1
         else:
             return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        """ Returns new instances to be squared shape. """
-        return cls(width=size, height=size)
