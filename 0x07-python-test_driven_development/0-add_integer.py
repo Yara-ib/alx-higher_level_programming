bf97192e2d2e60601669718ba5023f157e43003b
Yara-ib <yr.ap.23@gmail.com>
2023-11-29 15:10:50 +0200

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,17 +10,15 @@ def add_integer(a, b=98):
     """ Function to return the addition of 2 integers; arguments:
         a, b = int/float numbers (if b not mentioned = 98).
     """
-    # try:
-    if a is None or not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if b is None or not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    if (a + b) == float('inf') or (a + b) == -float('inf'):
-        return 89
-    # if not a and a != 0:
-    #     raise TypeError("missing 1 required positional argument: 'a'")
-    return int(a) + int(b)
+    try:
+        if not isinstance(a, int) and not isinstance(a, float):
+            raise TypeError("a must be an integer")
+        if not isinstance(b, int) and not isinstance(b, float):
+            raise TypeError("b must be an integer")
+        if not a and a != 0:
+            raise TypeError("missing 1 required positional argument: 'a'")
+        return int(a) + int(b)
 
-    # except NameError as ne:
-    #     unknown_variable = str(ne).split("'")[1]
-    #     print("NameError: name {} is not defined".format(unknown_variable))
+    except NameError as ne:
+        unknown_variable = str(ne).split("'")[1]
+        print("NameError: name {} is not defined".format(unknown_variable))
