dcdfffdbeb545e6eaffcfe3ae028473699a0fb9a
Yara-ib <yr.ap.23@gmail.com>
2023-11-29 15:09:08 +0200

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,17 +10,15 @@ def add_integer(a, b=98):
     """ Function to return the addition of 2 integers; arguments:
         a, b = int/float numbers (if b not mentioned = 98).
     """
-    if a != a:
-        a = 89
-    if b != b:
-        b = 89
-    if a is None or (type(a) is not int and type(a) is not float):
+    # try:
+    if a is None or not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if b is None or not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    result = a + b
-    if result == float('inf') or result == -float('inf'):
+    if (a + b) == float('inf') or (a + b) == -float('inf'):
         return 89
+    # if not a and a != 0:
+    #     raise TypeError("missing 1 required positional argument: 'a'")
     return int(a) + int(b)
 
     # except NameError as ne:
