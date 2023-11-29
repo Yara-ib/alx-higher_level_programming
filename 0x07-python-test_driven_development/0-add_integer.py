238f5eb3bbd13f32fc57d6f6dcfe051d8f11ce75
Yara-ib <yr.ap.23@gmail.com>
2023-11-29 15:39:52 +0200

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -18,9 +18,9 @@ def add_integer(a, b=98):
         if not a and a != 0:
             raise TypeError("missing 1 required positional argument: 'a'")
         if a + b == float("inf") or a + b == -float("inf"):
-            raise ValueError("Float overflow")
+            raise ValueError("OverflowError: cannot convert float infinity to integer")
         if a is "NAN" or b is "NAN":
-            # raise ValueError("cannot convert float NaN to integer")
+            raise ValueError("cannot convert float NaN to integer")
             return 89
         return int(a) + int(b)
 
