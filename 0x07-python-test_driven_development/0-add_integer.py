d21b621a823df1e3ab80114db90dda0aed1c7262
Yara-ib <yr.ap.23@gmail.com>
2023-11-29 15:24:29 +0200

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,6 +17,8 @@ def add_integer(a, b=98):
             raise TypeError("b must be an integer")
         if not a and a != 0:
             raise TypeError("missing 1 required positional argument: 'a'")
+        if a + b == float("inf") or a + b == -float("inf"):
+            return 89
         return int(a) + int(b)
 
     except NameError as ne:
