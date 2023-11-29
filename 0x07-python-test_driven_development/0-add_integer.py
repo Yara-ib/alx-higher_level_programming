6d69017d08a2e8694d106bcd5a70b7c3d3959de6
Yara-ib <yr.ap.23@gmail.com>
2023-11-29 15:35:32 +0200

    TDD

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,8 +17,10 @@ def add_integer(a, b=98):
             raise TypeError("b must be an integer")
         if not a and a != 0:
             raise TypeError("missing 1 required positional argument: 'a'")
-        if float('-inf') < float(a + b) < float('inf'):
+        if a + b == float("inf") or a + b == -float("inf"):
             return 89
+        if a is "NAN" or b is "NAN":
+            raise ValueError("cannot convert float NaN to integer")
         return int(a) + int(b)
 
     except NameError as ne:
