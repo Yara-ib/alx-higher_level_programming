8630fb1b4ad699a37c1b8ad23ed81b4253795b39
Yara-ib <yr.ap.23@gmail.com>
2023-11-29 16:12:41 +0200

    TDD

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -37,7 +37,7 @@ def matrix_mul(m_a, m_b):
     if not all(len(m_b[0]) == len(idx) for idx in m_b):
         raise TypeError("each row of m_b must be of the same size")
 
-    if (len(m_a) != len(m_b)):
+    if (len(m_a) != len(m_b)) or (len(m_a[0]) != len(m_b[0])) :
         raise ValueError("m_a and m_b can't be multiplied")
 
     error = "matrix_mul() missing 1 required positional argument: "
