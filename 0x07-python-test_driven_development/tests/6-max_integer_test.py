e42daed7315e37502242ab87c9709f706fd8d098
Yara-ib <yr.ap.23@gmail.com>
2023-12-02 04:13:24 +0200

    TDD

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,15 +6,11 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
-        self.assertTrue([1, 3, 4, 2], 4)
-
-    def test_max_integer2(self):
-        self.assertTrue([1, 3, 4, 22], 22)
-
-    def test_max_integer3(self):
-        self.assertTrue([11, 3, 4, 2], 11)
-        self.assertTrue([1, 3, 5, 2, 3], 5)
-        self.assertTrue([1, -3, 4, 2], 4)
-        self.assertTrue([-1, -3, -4, -2], -1)
-        self.assertTrue([1], 1)
-        self.assertFalse([], 0)
+        self.assertTrue(max_integer([1, 3, 4, 2]), 4)
+        self.assertTrue(max_integer([1, 3, 4, 22]), 22)
+        self.assertTrue(max_integer([11, 3, 4, 2]), 11)
+        self.assertTrue(max_integer([1, 3, 5, 2, 3]), 5)
+        self.assertTrue(max_integer([1, -3, 4, 2]), 4)
+        self.assertTrue(max_integer([-1, -3, -4, -2]), -1)
+        self.assertTrue(max_integer([1]), 1)
+        self.assertFalse(max_integer([]), 0)
