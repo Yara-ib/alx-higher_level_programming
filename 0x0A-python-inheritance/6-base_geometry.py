9a433339ea78ab674156419202d4a36e3fd09f97
Yara-ib <yr.ap.23@gmail.com>
2023-12-04 16:34:12 +0200

    Area on the way

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Geometry Module. """
+
+
+class BaseGeometry:
+    """ Empty class till further notice ;) """
+    def area(self):
+        """ Just raising Exception error."""
+        raise Exception("area() is not implemented")
