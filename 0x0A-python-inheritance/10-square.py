667752886786817523b4b371bc04f04e3db58cdc
Yara-ib <yr.ap.23@gmail.com>
2023-12-05 01:07:41 +0200

    sQuare

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Square #1 Module. """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        """ Initiating the objects.
+
+        Args:
+            size: square's side.
+
+        Returns:
+            created objects for square's sides.
+        """
+        Rectangle.__init__(Square, size, size)
+        self.__size = size
