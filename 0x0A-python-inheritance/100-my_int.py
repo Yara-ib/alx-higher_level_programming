a1389f59d4fff5e9be5a1edf49318f993cb5bb12
Yara-ib <yr.ap.23@gmail.com>
2023-12-04 23:26:02 +0200

    RectanGle

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class MyInt(int):
+    def my_i(i):
+        if i == i:
+            return (i != i)
+        # else:
+        #     return (i == i)
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Rectangle Module. """
+
+
+class BaseGeometry:
+    """ BaseGeometry class """
+    def area(self):
+        """ Just raising Exception error."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validating the value.
+
+        Args:
+            name: variable's name.
+            value: value to be checked.
+
+        Returns:
+            Errors depends on the case.
+        """
+        if not type(value) is int and isinstance(name, str):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0 and isinstance(name, str):
+            raise ValueError("{} must be greater than 0".format(name))
+        if not isinstance(name, str):
+            raise TypeError("{} must be an string".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle that inherits from BaseGeometry. """
+    def __init__(self, width, height):
+        """ Initiating the objects.
+
+        Args:
+            width: rectangle's width.
+            height: rectangle's height.
+
+        Returns:
+            created objects for width & height.
+        """
+        self.__width = width
+        self.__height = height
+
+        BaseGeometry().integer_validator("width", self.__width)
+        BaseGeometry().integer_validator("height", self.__height)
