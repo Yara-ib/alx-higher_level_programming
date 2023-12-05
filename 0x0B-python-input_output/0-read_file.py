a8540d0a254093a163536f1be9143df72ed9ea09
Yara-ib <yr.ap.23@gmail.com>
2023-12-05 23:10:58 +0200

    Read

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""" Read file Module. """
+
+
+def read_file(filename=""):
+    """ function that reads a text file.
+    Args:
+        filename: filename to be processed.
+    Return:
+        printed text from the file.
+    """
+    with open("my_file_0.txt", encoding="UTF8") as file:
+        print(file.read().rstrip())
