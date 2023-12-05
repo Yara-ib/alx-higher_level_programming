92b3eed43962bb906efedd4f88271304a884cd70
Yara-ib <yr.ap.23@gmail.com>
2023-12-06 01:01:24 +0200

    Loadclear

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Create object from a JSON file Module."""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    creates an Object from a “JSON file”.
+    Args:
+        filename: filename to be processed.
+    Return:
+        JSON's file's content.
+    """
+    with open(filename) as file:
+        return json.loads(file)
