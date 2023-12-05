846d2396258c0d60f3e2188b410504c0c9e4d1f6
Yara-ib <yr.ap.23@gmail.com>
2023-12-05 23:14:01 +0200

    Read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,4 +10,4 @@ def read_file(filename=""):
         printed text from the file.
     """
     with open("my_file_0.txt", encoding="utf-8") as file:
-        print(file.read().rstrip())
+        print(file.read(), end="")
