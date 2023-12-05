dc1eb6417c84827c88580c28311916bfaa9d0d5f
Yara-ib <yr.ap.23@gmail.com>
2023-12-06 00:24:56 +0200

    Object

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+     writes an Object to a text file, using a JSON representation.
+    Args:
+        my_obj: JSON's object that shall be added to the file.
+        filename: filename to be processed.
+    Return:
+        new JSON's file.
+    """
+    with open(filename, "w") as file:
+        file.write(json.dumps(my_obj))
