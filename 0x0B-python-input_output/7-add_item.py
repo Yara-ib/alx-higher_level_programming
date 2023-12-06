cb50578f338f24d3a321f89bd62341c942a30dbb
Yara-ib <yr.ap.23@gmail.com>
2023-12-06 04:08:22 +0200

    Whatever'll be 'll be

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Load, add, save Module."""
-import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+import sys
 
 
 filename = "add_item.json"
