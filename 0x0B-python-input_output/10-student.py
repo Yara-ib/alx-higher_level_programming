090e197cc5797083cc000d3d20b63049fbe45ba2
Yara-ib <yr.ap.23@gmail.com>
2023-12-06 04:45:03 +0200

    TasK

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Student to JSON Module. """
+
+
+class Student:
+    """ class Student that defines a student.
+
+    Attributes:
+        first_name: student's first name
+        last_name: student's last name
+        age: student's age
+    """
+    def __init__(self, first_name, last_name, age):
+        """ Instantiation new instances."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Retrieves a dictionary representation of a Student."""
+        if type(attrs) is list and all(isinstance(attr, str) for attr in attrs):
+            return self.__dict__
+        else:
+            return self.__dict__
