3f6a9b46ea1841e85ee5634bbefbd5be11c62e63
Yara-ib <yr.ap.23@gmail.com>
2023-12-07 04:53:04 +0200

    append

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 import sys
+"""  Log parsing Module. """
 
 
+get = "GET /projects/260 HTTP/1.1"
+list_argv = ["IP Address", "date", get, list_status_code, "file size"]
+
+# <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
 print(f"File size: {total size}")
 list_status_code = [200, 301, 400, 401, 403, 404, 405, 500]
 if status_code in sorted(list_status_code):
--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 Student = __import__('10-student').Student
+""" Student to disk and reload Module. """
+
 
 def reload_from_json(self, json):
+    """" replaces all attributes of the Student instance."""
     for key, value in self:
         self.update(key, value)
