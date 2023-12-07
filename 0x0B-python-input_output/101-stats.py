d8793fb2f607795af05bf9940f54254ef4273add
Yara-ib <yr.ap.23@gmail.com>
2023-12-07 04:51:46 +0200

    append

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+
+
+print(f"File size: {total size}")
+list_status_code = [200, 301, 400, 401, 403, 404, 405, 500]
+if status_code in sorted(list_status_code):
+    print(f"{status_code}: {number}")
--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -2,3 +2,5 @@
 Student = __import__('10-student').Student
 
 def reload_from_json(self, json):
+    for key, value in self:
+        self.update(key, value)
