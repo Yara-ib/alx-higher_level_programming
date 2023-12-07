52c1a8fc08298f18a31778f566200a7bec272616
Yara-ib <yr.ap.23@gmail.com>
2023-12-07 04:56:30 +0200

    append

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,9 +2,6 @@
 """  Log parsing Module. """
 
 
-import sys
-
-
 get = "GET /projects/260 HTTP/1.1"
 list_argv = ["IP Address", "date", get, list_status_code, "file size"]
 print(f"File size: {total size}")
