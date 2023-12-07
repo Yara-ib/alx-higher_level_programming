30cd5302a98aae2c496ba98d1537c8b0475c9eab
Yara-ib <yr.ap.23@gmail.com>
2023-12-07 04:53:53 +0200

    append

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-Student = __import__('10-student').Student
 """ Student to disk and reload Module. """
+
+
+Student = __import__('10-student').Student
 
 
 def reload_from_json(self, json):
