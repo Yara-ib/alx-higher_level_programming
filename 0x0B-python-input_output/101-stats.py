c5173d68bfba00098d76ea9f5a992db4a94d3ede
Yara-ib <yr.ap.23@gmail.com>
2023-12-07 05:01:51 +0200

    append

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ Log parsing Module. """
+import sys
 
 
-list_argv = ["IP Address", "date", get, list_status_code, "file size"]
-get = "GET /projects/260 HTTP/1.1"
 print(f"File size: {total size}")
 list_status_code = [200, 301, 400, 401, 403, 404, 405, 500]
 if status_code in sorted(list_status_code):
