7b9d75ba85212fee809b8c89709b6097ec691d10
Yara-ib <yr.ap.23@gmail.com>
2024-02-14 21:09:52 +0200

    Grant Wishes ;)

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,8 +1,3 @@
 -- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- CREATE USER user_0d_1@localhost IDENTIFIED WITH authentication_plugin BY '123';
-
--- CREATE USER user_0d_2@localhost IDENTIFIED WITH mysql_native_password BY 'G#5&vP!w2sY';
--- CREATE USER user_0d_1@localhost IDENTIFIED WITH mysql_native_password BY 'G#5&vP!w2sY';
