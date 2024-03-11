4c23d3fbb2c915d9ae9870fff7eb93579c5b1509
Yara-ib <yr.ap.23@gmail.com>
2024-03-12 01:46:54 +0200

    Weird Language 'Honestly'>> Yes

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.callMeMoby = function callMeMoby (x, theFunction) {
+exports.callMeMoby = function callMeMoby (x) {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
