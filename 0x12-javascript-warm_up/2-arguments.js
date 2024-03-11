90c09f7b6cae3ea8988d1b7b54f522b4633c179f
Yara-ib <yr.ap.23@gmail.com>
2024-03-11 17:56:56 +0200

    Arguments passe on CL

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const { argv } = require("node:process");
+const { argv } = require('node:process');
 argv.length > 3
-  ? console.log("Arguments found")
+  ? console.log('Arguments found')
   : argv.length > 2
-  ? console.log("Argument found")
-  : console.log("No argument");
+    ? console.log('Argument found')
+    : console.log('No argument');
