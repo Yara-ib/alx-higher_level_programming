9a99559671251870eea15e4a14e9b6b6ef387333
Yara-ib <yr.ap.23@gmail.com>
2024-03-12 02:56:03 +0200

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+function factorial (x) {
+  if (x == 0 || x == 1 || isNaN(x)) {
+    return 1;
+  } else {
+    return (x * factorial(x - 1));
+  }
+}
+
+console.log(factorial(argv[2]));
