a4eca10fbdfcaaf57086635f8c9b0619bc966438
Yara-ib <yr.ap.23@gmail.com>
2024-03-12 03:01:15 +0200

    2nd Biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
+// const { argv } = require('node:process');
+
+const argv = process.argv.slice(2);
+
 argv.length > 3 ? console.log(argv.sort().reverse()[1]) : console.log(0);
