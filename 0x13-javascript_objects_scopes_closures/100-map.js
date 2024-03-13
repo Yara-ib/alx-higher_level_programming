de9f4b807ea13f64fd0f5d95bd61d27a222a625e
Yara-ib <yr.ap.23@gmail.com>
2024-03-13 02:28:07 +0200

    Map()

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Script that imports an array and computes a new array.
+const list = require('./100-data.js').list;
+console.log(list);
+
+const arr2 = list.map((x) => x * list.indexOf(x));
+console.log(arr2);
