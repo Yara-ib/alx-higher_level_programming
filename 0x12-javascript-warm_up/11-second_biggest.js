b9de8cadf274e0fdd07dbf709aefafce7be340d5
Yara-ib <yr.ap.23@gmail.com>
2024-03-13 20:19:08 +0200

    Compare function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,4 +2,14 @@
 
 const { argv } = require('node:process');
 
-argv.length > 3 ? console.log(argv.sort().reverse()[1]) : console.log(0);
+// Comparison Function been added because sort() only sort by strings
+// So 4 will be > 11 which is incorrect
+argv.length > 3
+  ? console.log(
+    argv
+      .sort(function (a, b) {
+        return a - b;
+      })
+      .reverse()[1]
+  )
+  : console.log(0);
