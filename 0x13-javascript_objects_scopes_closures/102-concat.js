f2c1212c4f2fd4968bbdcbf9811ea32c108d4737
Yara-ib <yr.ap.23@gmail.com>
2024-03-13 17:49:18 +0200

    Reading & writing Files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+const fs = require('node:fs');
+
+fs.readFile(argv[2], 'utf8', (err, data) => {
+  error(err);
+  fs.writeFile('./fileC', data, (err) => {
+    error(err);
+  });
+});
+
+fs.readFile(argv[3], 'utf8', (err, data) => {
+  error(err);
+  fs.writeFile('./fileC', data, { flag: 'a+' }, (err) => {
+    error(err);
+  });
+});
+
+function error (err) {
+  if (err) {
+    console.log(err);
+  }
+}
