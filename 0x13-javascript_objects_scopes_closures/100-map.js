85005f67e28e50be87244878e28e4131655219eb
Yara-ib <yr.ap.23@gmail.com>
2024-03-13 02:33:28 +0200

    Map()

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -4,8 +4,5 @@
 const list = require('./100-data.js').list;
 console.log(list);
 
-// const arr2 = list.map((x) => x * list.indexOf(x));
-// console.log(arr2);
-
 const arr = list.map((value, index) => value * index);
 console.log(arr);
