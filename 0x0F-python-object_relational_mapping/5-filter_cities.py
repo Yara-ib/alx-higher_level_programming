c51baf42c15dce7029864649bc188225c1918c8f
Yara-ib <yr.ap.23@gmail.com>
2024-03-16 01:56:02 +0200

    Filter Cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
         'SELECT cities.name FROM `cities`'
         'JOIN `states`'
         'ON cities.state_id = states.id WHERE BINARY states.name = "{}"'
+        'ORDER BY cities.id'
         .format(argv[4])
     )
 
