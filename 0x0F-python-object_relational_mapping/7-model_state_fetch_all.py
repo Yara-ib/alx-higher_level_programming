76a99dc8205c8dd6dbbb2ba56b0e151aaa31a71c
Yara-ib <yr.ap.23@gmail.com>
2024-03-16 03:05:51 +0200

    Fetch data

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Class definition of a State """
+""" Lists all State objects from the database hbtn_0e_6_usa """
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -14,6 +14,7 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     session = Session()
+
     output = session.query(State).order_by(State.id)
     for row in output:
         print(row)
