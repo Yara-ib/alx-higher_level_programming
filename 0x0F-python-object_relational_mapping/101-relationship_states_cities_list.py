1eaf0896c3bf18fe39672efe6d357cc32b5e6da3
Yara-ib <yr.ap.23@gmail.com>
2024-03-18 18:48:26 +0200

    List Cities

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -15,6 +15,7 @@ if __name__ == "__main__":
         'mysql+mysqldb://{}:{}@localhost/{}'
         .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
         )
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
