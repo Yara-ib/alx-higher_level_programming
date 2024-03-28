1c145f42dd286b36d379ea2c24c5eb7528134235
Yara-ib <yr.ap.23@gmail.com>
2024-03-28 21:11:33 +0200

    Deleting xtra comments

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,9 @@
 
 if __name__ == '__main__':
     from urllib import request
+
     with request.urlopen('https://alx-intranet.hbtn.io/status') as url:
         readable = url.read()
-        # for line in readable:
-        #     print(line)
         print(f'Body response:\n\t- type: {type(readable)}')
         print(f'\t- content: {readable}')
         print(f'\t- utf8 content: {readable.decode("utf-8")}')
