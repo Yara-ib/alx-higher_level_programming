5f86ecdf23e4b340119d10a5e1882d54823ece94
Yara-ib <yr.ap.23@gmail.com>
2024-03-28 20:47:41 +0200

    Status of URL

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""  Python script that fetches https://alx-intranet.hbtn.io/status """
+
+
+if __name__ == '__main__':
+    from urllib import request
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as url:
+        readable = url.read()
+        # for line in readable:
+        #     print(line)
+        print(f'Body response:\n\t- type: {type(readable)}')
+        print(f'\t- content: {readable}')
+        print(f'\t- utf8 content: {readable.decode("utf-8")}')
