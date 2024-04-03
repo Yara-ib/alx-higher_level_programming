d70f9f66de5dec4eb9b391e994f154e77a13d657
Yara-ib <yr.ap.23@gmail.com>
2024-04-04 01:43:17 +0200

    Header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-""" Script sends a request to URL & displays value of variable in response header """
+""" Script sends request & get value of variable in response header """
 
 
 if __name__ == '__main__':
     import requests
     from sys import argv
 
-    try:
-        request = requests.get(argv[1])
-        print(request.headers.get('X-Request-Id'))
-
-    except Exception as e:
-        pass
+    request = requests.get(argv[1])
+    print(request.headers.get('X-Request-Id'))
